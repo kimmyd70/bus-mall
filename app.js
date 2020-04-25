ebf6b9361dbca316f5ad34976af617e9e0eb9e1a
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-04-25 15:05:01 -0700

    set up

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 'use strict';
 
 //US #1 Display 3 unique (random) products
-//User pics favorite with click
 //Create constructor properties: product-name, image file path
+
+function productAvailable (name, photo){
+  this.productName = name;
+  this.source = photo;
+
+  this.clickCount = 0;
+  this.showCount = 0;
+}
+
+
 //Randomize and show 3 products (no doubles)
 //render side-by-side
 //Attach event listener
-//repeat on click
+//User pics favorite with click, repeat to var rounds = 25
 
 //US #2 Track selections made to determine most picked
 // Constructor property: clicked
@@ -17,4 +26,6 @@
 
 //US #4 view a report of results
 // Constructor property: shown to customer
-//Display: product, votes, times shown--in popular order--table?
+//Display: productName, clickCount, showCount--in popular order--table?
+
+var tableHead = 
