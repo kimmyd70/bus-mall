7431bd6e63f309acb1643b5467a1ba3179f338c7
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-04-25 14:32:50 -0700

    skeleton built

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//US #1 Display 3 unique (random) products
+//User pics favorite with click
+//Create constructor properties: product-name, image file path
+//Randomize and show 3 products (no doubles)
+//render side-by-side
+//Attach event listener
+//repeat on click
+
+//US #2 Track selections made to determine most picked
+// Constructor property: clicked
+
+//US #3 max 25 rounds of voting
+//loop 25....use global variable for counter to easily replace
+//remove listener
+
+//US #4 view a report of results
+// Constructor property: shown to customer
+//Display: product, votes, times shown--in popular order--table?
