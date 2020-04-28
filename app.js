ced0f17159e454805842d6ad1ad8047011dd978c
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-04-27 18:18:41 -0700

    fix clickCount, round max, and display table

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 //Global Variables
 var products = [];
+var votingRounds = 5;
 
 //DOM links
 
@@ -15,14 +16,14 @@ var sectionEl = document.getElementById('product-display');
 //US #1 Display 3 unique (random) products
 //Create constructor properties: product-name, image file path
 
-function ProductAvailable (name, source){
-  this.productName = name;
-  this.photo = source;
+function ProductAvailable (name, src){
+  this.name = name;
+  this.src = src;
 
   this.clickCount = 0;
   this.showCount = 0;
 
-  products.push (this);
+  products.push(this);
 }
 
 //Instantiate products
@@ -57,19 +58,17 @@ function randomIndex (max){
 // ...and show 3 products (no doubles) side by side; update show Count
 
 function display(){
-  var index1 = 0;
-  var index2 = 0;
-  var index3 = 0;
 
-  //while ((index1 === index2) || (index1 === index3) || (index2 === index3)){
-    index1 = randomIndex(products.length);
-    index2 = randomIndex(products.length);
-    index3 = randomIndex(products.length);
-  //}
+  while ((index1 === index2) || (index1 === index3) || (index2 === index3)){
+    var index1 = randomIndex(products.length);
+    var index2 = randomIndex(products.length);
+    var index3 = randomIndex(products.length);
+  }
+
   //set images to be displayed
-  picOneEl = products[index1].src;
-  picTwoEl = products[index2].src;
-  picThreeEl = products[index3].src;
+  picOneEl.src = products[index1].src;
+  picTwoEl.src = products[index2].src;
+  picThreeEl.src = products[index3].src;
 
   //set titles
   picOneEl.title = products[index1].name;
@@ -84,8 +83,8 @@ function display(){
 //US #2 Track selections made to determine most picked
 //handler function; update clicks; displays 3 new products
 function handleChoice(event){
-  var chosenProduct = event.target.title;
-  for (var i = 0; i , products.length; i++){
+  var chosenProduct = event.target.name;
+  for (var i = 0; i < products.length; i++){
     if (chosenProduct === products[i].name){
       products[i].clickCount++;
     }
@@ -94,14 +93,30 @@ function handleChoice(event){
 }
 
 //Attach event listener
-//User pics favorite with click, repeat to var rounds = 25
-sectionEl.addEventListener('click', handleChoice);
+//User pics favorite with click, repeat to var votingRounds = #
 
+function setRounds(rounds){
+  for (var i = 0; i < rounds; i++){
+    sectionEl.addEventListener('click', handleChoice);
+  }
+  sectionEl.removeEventListener;
 
-//US #3 max 25 rounds of voting
-//loop 25....use global variable for counter to easily replace
-//remove listener
+  //show bus-mall-logo after rounds complete
+  picOneEl.src = './assets/bus-mall-logo.png';
+  picTwoEl.src = './assets/bus-mall-logo.png' ;
+  picThreeEl.src = './assets/bus-mall-logo.png';
+  //invoke results report
+}
+
+//US #3 max 25 rounds of voting then remove listener
+//display first 3 then start and stop the event listener
+picOneEl.src = './assets/bus-mall-logo.png';
+picTwoEl.src = './assets/bus-mall-logo.png' ;
+picThreeEl.src = './assets/bus-mall-logo.png';
+
+setRounds(votingRounds);
+
 
 //US #4 view a report of results AFTER 25 votes
-// Constructor property: shown to customer
+
 //Display: productName, clickCount, showCount--in popular order--table?
